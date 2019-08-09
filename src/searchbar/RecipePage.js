c7e2559a75929cb25c0ab9cd00662267e3d97506
Ryan Kaw <rmkaw@asu.edu>
2019-08-08 21:06:47 -0700

    3rd api call added

---
+++ src/searchbar/RecipePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class RecipePage extends React.Component {
+   
+     
+render() {
+    console.log(this.props)
+    return (
+     <div>
+         t
+     </div>   
+    )
+}
+}
+
+export default RecipePage;
--- src/searchbar/RecipeResults.js
+++ src/searchbar/RecipeResults.js
@@ -1,38 +1,52 @@
 import React from 'react';
+import RecipePage from './RecipePage'
 
 class RecipeResults extends React.Component {
-   
+
     constructor(props){
         super(props);
-       /* this.state = {
-          meal: '',
-          mealPicture: '',
-          mealItemID: ''
-        }*/
-    }    
-    resultsMap(){
+        this.state = {
+          id: ' ',
+          recipe: null
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(event){
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${event.target.id}`)
+            .then(response => response.json())
+            .catch(error => {throw new Error(error.message)})
+            .then(data => {
+                console.log(data)
+                console.log("I have the data")
+                this.setState({recipe: data})
+              });
+              console.log(this.state.recipe);
+              event.preventDefault();
+    }
+    
+     resultsMap(){
         const resultsList = this.props.results.meals;
         let resultsArray = []
         if (resultsList) {
           resultsList.map((res) =>{
+
           resultsArray.push(<div>
               <p>{res.strMeal}</p>
               <img src={res.strMealThumb} />
+              <button id={res.idMeal} onClick={this.handleClick}>Gib Food Pls</button>
           </div>)
-        })
+         }
+        )
       }
         return resultsArray;
       }
 
-
- componentDidMount(){
-
-    }
-
 render() {
     return (
      <div>
-         {this.resultsMap()}
+         {this.resultsMap()} 
+         <RecipePage recipe={this.state.recipe}/>       
      </div>   
     )
 }
