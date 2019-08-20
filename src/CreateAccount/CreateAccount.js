394270008c7128af3618289314ed693c0ae8e0ee
ryan6921 <rmkaw@asu.edu>
2019-08-19 17:40:37 -0700

    some more auth additions

--- src/CreateAccount/CreateAccount.js
+++ src/CreateAccount/CreateAccount.js
@@ -2,12 +2,26 @@ import React from 'react';
 
 class CreateAccount extends React.Component {
 
- 
+handleCreate(user) {
+        return fetch(`${config.API_ENDPOINT}/users`, {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify(user),
+        })
+          .then(res =>
+            (!res.ok)
+              ? res.json().then(e => Promise.reject(e))
+              : res.json()
+          )
+      }
+
      
 render() {
     return (
     <div>
-   <form name="login" action="index_submit" method="get" accept-charset="utf-8" onSubmit={this.handleSubmitJwtAuth}>
+   <form name="login" action="index_submit" method="get" accept-charset="utf-8">
 		<ul>
 			<li>
 				<label for="usermail">Email</label>
@@ -22,7 +36,7 @@ render() {
 				<input type="password" name="password" placeholder="password" required />
             </li>
 			<li>
-				<input type="submit" value="Create Account" />
+				<input type="submit" value="Create Account" onSubmit={this.handleCreate}/>
 			</li>
 		</ul>
 	        </form>
