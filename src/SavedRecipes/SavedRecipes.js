405de106af04d5c6309218658b7edcc267d1176e
ryan6921 <rmkaw@asu.edu>
2019-08-22 16:55:09 -0700

    added details for saved recipes

--- src/SavedRecipes/SavedRecipes.js
+++ src/SavedRecipes/SavedRecipes.js
@@ -15,22 +15,21 @@ constructor(props){
     this.deleteRecipe = this.deleteRecipe.bind(this);
     this.savedResultsPage = this.savedResultsPage.bind(this);
     this.recipePhotoLink = this.recipePhotoLink.bind(this);
+    this.revertList = this.revertList.bind(this);
 }
 
 deleteRecipe (e){
     e.preventDefault();
     let recipeID = parseInt(e.target.id)
     let token = TokenService.getAuthToken();
-  
     fetch(`http://localhost:8000/api/recipes/${recipeID}`, {
     method: 'DELETE',
     mode: 'cors',
     headers: {
         'Content-Type': 'application/json',
         'Authorization' : `Bearer ${token}`
-    }
-
-  })
+      }
+    })
     .then(res => {
       if (!res.ok) {
         return res.json().then(error => {
@@ -49,8 +48,7 @@ deleteRecipe (e){
 
 
 componentDidMount() {
-  let token = TokenService.getAuthToken();
-
+    let token = TokenService.getAuthToken();
     fetch('http://localhost:8000/api/recipes', {
       method: 'GET',
       mode: 'cors',
@@ -58,14 +56,11 @@ componentDidMount() {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`
       }
-      
     })
     .then(response=> response.json())
     .then(data => {
-        console.log("I have the data for Saved Recipes")
         this.setState({recipes: data})})
-        .catch(error => {throw new Error(error.message)})
-        ;
+        .catch(error => {throw new Error(error.message)});
 }
 
 savedResultsMap(){
@@ -90,16 +85,39 @@ savedResultsMap(){
 recipePhotoLink(event){
   this.setState({ showDetails: true, selectedRecipeId: parseInt(event.target.id)})
 }
+revertList(){
+  this.setState({ showDetails: false})
+}
+
 savedResultsPage (){
      const recipes = this.state.recipes
      const selectedRecipe = recipes.filter(recipe => recipe.id === this.state.selectedRecipeId)[0];
-     console.log(selectedRecipe)
-     return <div>Details here</div>
+     let ingredients = selectedRecipe.ingredients.split(',')
+     let list = ingredients.map(ingredient =>{
+        if (ingredient !== '' && ingredient !== 'null'){
+          return <li>{ingredient}</li>
+        }
+     })
+     return  <div>
+     <p name="recipe-title" value={selectedRecipe.recipe_title}>{selectedRecipe.recipe_title}</p>
+     <img alt='A picture of the completed recipe.' name="recipe-picture" value={selectedRecipe.picture} src={selectedRecipe.picture} />
+     <p name="recipe-region" value={selectedRecipe.region}>Region: {selectedRecipe.region}</p>
+     <p name="recipe-instructions"  value={selectedRecipe.instructions}>{selectedRecipe.instructions}</p>
+     <div>Ingredients</div>
+     <ul name="recipe-ingredients">
+       {list}
+     </ul>
+
+     <p>Can't read? Don't wanna read?</p>
+     <video name="recipe-video"  value={selectedRecipe.video}  width="120" height="80" controls>
+         <source src={selectedRecipe.video} />
+      </video>
+      <button type="submit" onClick={this.revertList}>Back to your list </button>
+ </div>
 }
 
      
 render() {
-  console.log("in saved recipes")
     return (
     <div>
        {this.state.showDetails ? this.savedResultsPage() : this.savedResultsMap()}
